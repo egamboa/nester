37ee5ad641eedb0669015b3a6d2859ed6f547bee
Esteban Gamboa <estebangam1@gmail.com>
2018-06-04 20:42:28 -0600

    wip

---
+++ nesterapp/nesterapp/models/hotel.swift
@@ -0,0 +1,9 @@
+//
+//  hotels.swift
+//  nesterapp
+//
+//  Created by Andrés Gamboa on 5/26/18.
+//  Copyright © 2018 Andrés Gamboa. All rights reserved.
+//
+
+import Foundation
---
+++ nesterapp/nesterapp/models/room.swift
@@ -0,0 +1,9 @@
+//
+//  rooms.swift
+//  nesterapp
+//
+//  Created by Andrés Gamboa on 5/26/18.
+//  Copyright © 2018 Andrés Gamboa. All rights reserved.
+//
+
+import Foundation
